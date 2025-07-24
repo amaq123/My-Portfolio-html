d6db0608b42ef3e1c1434f45159271431b2d39fe
amaq123 <117670221+amaq123@users.noreply.github.com>
2025-07-24 18:27:24 +0500

    changing in js

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-console.log("Script...")
+window.onload = () => {
+  console.log("Script loaded...");
 
-document.querySelector('.cross').style.display = 'none';
+  document.querySelector('.cross').style.display = 'none';
 
-document.querySelector('.hamburger').addEventListener("click", () => {
+  document.querySelector('.hamburger').addEventListener("click", () => {
     document.querySelector('.sidebar').classList.toggle('sidebarGo');
 
     if (document.querySelector('.sidebar').classList.contains('sidebarGo')) {
-        document.querySelector('.ham').style.display = 'inline';
-        document.querySelector('.cross').style.display = 'none';
+      document.querySelector('.ham').style.display = 'inline';
+      document.querySelector('.cross').style.display = 'none';
     } else {
-        document.querySelector('.cross').style.display = 'inline';
-        setTimeout(() =>{
-            document.querySelector('.ham').style.display = 'none';
-        }, 300);
-        
+      document.querySelector('.cross').style.display = 'inline';
+      setTimeout(() => {
+        document.querySelector('.ham').style.display = 'none';
+      }, 300);
     }
-});
+  });
+};
